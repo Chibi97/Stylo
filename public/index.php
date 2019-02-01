b91788742d91713b2f0963aefb1754de25b157b7
Olja Ivkovic <olja.ivkovic97@gmail.com>
2019-02-01 15:52:11 +0100

    setup views and navigation through page parameter

--- public/index.php
+++ public/index.php
@@ -6,21 +6,42 @@
 	<?php include_once "views/head.php"; ?>
 	 
 <body class="animsition">
-		<!-- Header -->
-  <?php include_once "views/header.php"; ?>
+  <?php include_once "views/navigation.php"; ?>
 
 	<?php 
 	$page = null;
 	if (isset($_GET['page'])) {
 		$page = $_GET['page'];
-	} ?>
-
-	<?php 
-	include_once "views/slider.php";
-	include_once "views/featured_products.php";
-	include_once "views/advertising.php";
+	} 
+	
+	switch($page) {
+		case 'shop':
+			include_once "views/all-products.php";
+			break;
+		case 'popular':
+			include_once "views/featured-products.php";
+			break;
+	  case 'about':
+			include_once "views/about.php";
+			break;
+		case 'contact':
+			include_once "views/contact.php";
+			break;
+		case 'cart':
+			include_once "views/cart.php";
+			break;
+		case 'product':
+			include_once "views/product-details.php";
+			break;
+		default:
+			include_once "views/slider.php";
+			include_once "views/featured-products.php";
+			include_once "views/advertising.php";
+		  break;
+	}
+	
 	include_once "views/footer.php";
-	require_once "views/scripts.php" ?>
+	require_once "views/scripts.php"; ?>
 
 </body>
 </html>
---
+++ public/views/navigation.php
@@ -0,0 +1,168 @@
+	<!-- Header -->
+	<header class="header1">
+		<!-- Header desktop -->
+		<div class="container-menu-header">
+			<div class="topbar">
+				<div class="topbar-social">
+					<a href="#" class="topbar-social-item fab fa-facebook-f"></a>
+					<a href="#" class="topbar-social-item fab fa-instagram"></a>
+					<a href="#" class="topbar-social-item fab fa-pinterest-p"></a>
+					<a href="#" class="topbar-social-item fab fa-snapchat-ghost"></a>
+					<a href="#" class="topbar-social-item fab fa-youtube"></a>
+				</div>
+
+				<span class="topbar-child1">
+					It's all about details.
+				</span>
+
+				<div class="topbar-child2">
+					<span class="topbar-email">
+						Admin linkovi, login, register
+					</span>
+				</div>
+			</div>
+
+			<div class="wrap_header">
+				<!-- Logo -->
+				<a href="index.html" class="logo">
+					<img src="images/icons/logo.png" alt="IMG-LOGO">
+				</a>
+
+				<!-- Menu -->
+				<div class="wrap_menu">
+					<nav class="menu">
+						<ul class="main_menu">
+							<li>
+								<a href="index.php">Home</a>
+							</li>
+
+							<li>
+                <a href="index.php?page=shop">Shop</a>
+                <!-- <ul class="sub_menu">
+									<li><a href="#">FOR HER</a></li>
+									<li><a href="#">FOR HIM</a></li>
+								</ul> -->
+							</li>
+
+							<li>
+								<a href="index.php?page=popular">Most popular items</a>
+							</li>
+
+							<li>
+								<a href="index.php?page=about">About author</a>
+							</li>
+
+							<li>
+								<a href="index.php?page=contact">Contact</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+
+				<!-- Header Icon -->
+				<div class="header-icons">
+					<a href="#" class="header-wrapicon1 dis-block">
+						<img src="images/icons/icon-header-01.png" class="header-icon1" alt="ICON">
+					</a>
+
+					<span class="linedivide1"></span>
+
+					<div class="header-wrapicon2">
+					  <a href="index.php?page=cart">
+							<img src="images/icons/icon-header-02.png" class="header-icon1 js-show-header-dropdown" alt="ICON">
+							<span class="header-icons-noti">0</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Header Mobile -->
+		<div class="wrap_header_mobile">
+			<!-- Logo moblie -->
+			<a href="index.html" class="logo-mobile">
+				<img src="images/icons/logo.png" alt="IMG-LOGO">
+			</a>
+
+			<!-- Button show menu -->
+			<div class="btn-show-menu">
+				<!-- Header Icon mobile -->
+				<div class="header-icons-mobile">
+					<a href="#" class="header-wrapicon1 dis-block">
+						<img src="images/icons/icon-header-01.png" class="header-icon1" alt="ICON">
+					</a>
+
+					<span class="linedivide2"></span>
+
+					<div class="header-wrapicon2">
+					 <a href="index.php?page=cart">
+							<img src="images/icons/icon-header-02.png" class="header-icon1 js-show-header-dropdown" alt="ICON">
+							<span class="header-icons-noti">0</span>
+						</a>
+					</div>
+				</div>
+
+				<div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+					<span class="hamburger-box">
+						<span class="hamburger-inner"></span>
+					</span>
+				</div>
+			</div>
+		</div>
+
+		<!-- Menu Mobile -->
+		<div class="wrap-side-menu" >
+			<nav class="side-menu">
+				<ul class="main-menu">
+					<li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
+						<span class="topbar-child1">
+							It's all about details.
+						</span>
+					</li>
+
+					<li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
+						<div class="topbar-child2-mobile">
+							<span class="topbar-email">
+								Admin, login, register
+							</span>
+						</div>
+					</li>
+
+					<li class="item-topbar-mobile p-l-10">
+						<div class="topbar-social-mobile">
+							<a href="#" class="topbar-social-item fab fa-facebook-f"></a>
+							<a href="#" class="topbar-social-item fab fa-instagram"></a>
+							<a href="#" class="topbar-social-item fab fa-pinterest-p"></a>
+							<a href="#" class="topbar-social-item fab fa-snapchat-ghost"></a>
+							<a href="#" class="topbar-social-item fab fa-youtube"></a>
+						</div>
+					</li>
+
+					<li class="item-menu-mobile">
+						<a href="index.html">Home</a>
+					</li>
+
+					<li class="item-menu-mobile">
+            <a href="product.html">Shop</a>
+            <!-- <ul class="sub-menu">
+							<li><a href="index.html">FOR HER</a></li>
+							<li><a href="home-02.html">FOR HIM</a></li>
+						</ul> -->
+						<i class="arrow-main-menu fa fa-angle-right" aria-hidden="true"></i>
+					</li>
+
+					<li class="item-menu-mobile">
+						<a href="product.html">Most popular items</a>
+					</li>
+          
+					<li class="item-menu-mobile">
+						<a href="about.html">About author</a>
+					</li>
+
+					<li class="item-menu-mobile">
+						<a href="contact.html">Contact</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</header>
